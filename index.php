2a7552ead115f5c2fa2edb5d409f363cc2c42c70
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2026-02-13 18:04:01 +0200

    project beginnings

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calender project</title>
+    <meta name="description" content="A simple calendar project built with HTML, CSS, and JavaScript.">
+    <meta name="keywords" content="calendar, project, HTML, CSS, JavaScript">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>🗓️Calendar Project</h1>
+    </header>
+
+    <!-- clock -->
+    <div class="clock-container">
+        <div id="clock"></div>
+    </div>
+    
+    
+</body>
+
+
+</html>
